1658bf64af4cac7f77c6fe09a7b6f70118cb84ab
9BadWolf <keni.annie@gmail.com>
2025-01-08 23:34:48 +0200

    formating

--- IPLookup/Common/ValidateIPFilter.cs
+++ IPLookup/Common/ValidateIPFilter.cs
@@ -1,17 +1,13 @@
-﻿using System.Net;
-using IPLookup.Common.Results;
+﻿using IPLookup.Common.Results;
 
 namespace IPLookup.Common;
 
-public class ValidateIPFilter: IEndpointFilter
+public class ValidateIPFilter : IEndpointFilter
 {
     public async ValueTask<object?> InvokeAsync(EndpointFilterInvocationContext context, EndpointFilterDelegate next)
     {
         var ipAddress = context.GetArgument<string>(0);
-        if (!TryParseIPv4(ipAddress, out _))
-        {
-            return new InvalidProblem("Invalid IP Address.");
-        }
+        if (!TryParseIPv4(ipAddress, out _)) return new InvalidProblem("Invalid IP Address.");
         return await next(context);
     }
 
@@ -19,21 +15,14 @@ public class ValidateIPFilter: IEndpointFilter
     {
         validIp = string.Empty;
 
-        if (string.IsNullOrWhiteSpace(ipString))
-        {
-            return false;
-        }
+        if (string.IsNullOrWhiteSpace(ipString)) return false;
 
         var splitValues = ipString.Trim().Split('.');
-        if (splitValues.Length != 4)
-        {
-            return false;
-        }
+        if (splitValues.Length != 4) return false;
 
-        if (splitValues.Any(segment => !byte.TryParse(segment, out _) || (segment.Length > 1 && segment.StartsWith("0"))))
-        {
+        if (splitValues.Any(
+                segment => !byte.TryParse(segment, out _) || (segment.Length > 1 && segment.StartsWith("0"))))
             return false;
-        }
 
         validIp = ipString.Trim();
         return true;
